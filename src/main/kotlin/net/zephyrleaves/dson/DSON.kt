91744c80901cd2565e5c017a7b6c347498b87d48
wentong <wentong@mogujie.com>
2018-11-12 15:01:42 +0800

    重构一把

--- src/main/kotlin/net/zephyrleaves/dson/DSON.kt
+++ src/main/kotlin/net/zephyrleaves/dson/DSON.kt
@@ -13,32 +13,7 @@ fun dson(init: DSON.() -> Unit): DSON {
     return dson
 }
 
-class DSON : Node() {
-    fun obj(key: String, init: ObjectKey.() -> Unit) {
-        val myKey = ObjectKey(key)
-        myKey.init()
-        children.add(myKey)
-    }
-
-
-    fun array(key: String, init: ArrayKey.() -> Unit) {
-        val myKey = ArrayKey(key)
-        myKey.init()
-        children.add(myKey)
-    }
-
-    fun value(key: String, value: () -> Any?) {
-        val myKey = ValueKey(key, value())
-        children.add(myKey)
-    }
-
-    fun value(key: String, value: Any?) {
-        val myKey = ValueKey(key, value)
-        children.add(myKey)
-    }
-
-
-}
+class DSON : Node(), ObjectTrait
 
 
 
