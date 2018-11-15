a609184c080c493b3943d4c46e61e74e7a18cb3f
wentong <wentong@mogujie.com>
2018-11-15 19:19:20 +0800

    优化一下

--- src/main/kotlin/net/zephyrleaves/dson/DSON.kt
+++ src/main/kotlin/net/zephyrleaves/dson/DSON.kt
@@ -1,5 +1,9 @@
 package net.zephyrleaves.dson
 
+import org.jetbrains.kotlin.script.jsr223.KotlinJsr223JvmLocalScriptEngine
+import javax.script.CompiledScript
+import javax.script.ScriptEngineManager
+
 /**
  * @author kevin.huang
  * @since 2018/11/8 18:28
@@ -19,8 +23,16 @@ fun arr(init: ArrayValue.() -> Unit): ArrayValue {
     return dson
 }
 
+inline fun <reified T> Any?.safeAs(): T? =
+        if (this is T)
+            this
+        else
+            null
+
 
 object DSON {
+
+    internal val ScriptEngine = ScriptEngineManager().getEngineByExtension("kts") as KotlinJsr223JvmLocalScriptEngine
 
     fun from(v: ObjectValue): Map<String, Any?> {
         return v.data()
@@ -34,8 +46,23 @@ object DSON {
         return when (v) {
             is ObjectValue -> from(v)
             is ArrayValue -> from(v)
-            else -> throw Exception("v only is ObjectValue or ArrayValue")
+            else -> throw Exception("Value can only be ObjectValue or ArrayValue!")
         }
+    }
+
+    fun compile(code: String): DSONCompileContext {
+        return DSONCompileContext(ScriptEngine.compile(code))
+
+    }
+}
+
+
+class DSONCompileContext(private val compiledScript: CompiledScript) {
+
+    fun eval(bindings: Map<String, Any?>): Any {
+        val b = DSON.ScriptEngine.createBindings()
+        b.putAll(bindings)
+        return compiledScript.eval(b)
     }
 }
 
--- src/test/kotlin/net/zephyrleaves/dson/DSONKtTest.kt
+++ src/test/kotlin/net/zephyrleaves/dson/DSONKtTest.kt
@@ -3,12 +3,10 @@ package net.zephyrleaves.dson
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
-import org.jetbrains.kotlin.script.jsr223.KotlinJsr223JvmLocalScriptEngine
 import org.testng.annotations.Test
-import javax.script.ScriptEngineManager
 
 /**
- * @author zephyrleaves
+ * @author kevin.huang
  * @since 2018/11/9 14:33
  */
 class DSONKtTest {
@@ -45,27 +43,27 @@ class DSONKtTest {
             val payload = outerPayload
             v("default", payload)
             arr("my_array") {
-                (payload["array"] as List<Int>).forEach {
+                payload["array"].safeAs<List<Int>>()?.forEach {
                     v(it + 1)
                 }
             }
-            v("sum", (payload["array"] as List<Int>).sum())
+            v("sum", payload["array"].safeAs<List<Int>>()?.sum())
             v("test big_data") {
-                hashMapOf("1" to "11", "2" to "22") + payload["complex"] as Map<*, *>
+                mapOf("1" to "11", "2" to "22") + payload["complex"].safeAs<Map<*, *>>()!!
             }
             arr("all_array") {
                 v(100)
                 v(101)
                 v(102)
-                (payload["array"] as List<Int>).forEach { v ->
+                payload["array"].safeAs<List<Int>>()?.forEach { v ->
                     v(v + 1000)
                 }
                 v(2000)
-                for (v in payload["array"] as List<Int>) {
+                payload["array"].safeAs<List<Int>>()?.forEach { v ->
                     v(v + 2000)
                 }
                 v(3000)
-                v { hashMapOf("1" to "11", "2" to "22") + payload["complex"] as Map<*, *> }
+                v { hashMapOf("1" to "11", "2" to "22") + payload["complex"].safeAs<Map<*, *>>()!! }
                 obj {
                     v("over", true)
                 }
@@ -89,11 +87,8 @@ class DSONKtTest {
     @Test
     fun testScript() {
         val resource = this::class.java.classLoader.getResourceAsStream("simple.txt")
-        val se = ScriptEngineManager().getEngineByExtension("kts") as KotlinJsr223JvmLocalScriptEngine
-        val bindings = se.createBindings()
-        bindings["outerPayload"] = outerPayload
-        val compiler = se.compile(resource.bufferedReader(Charsets.UTF_8))
-        val obj = compiler.eval(bindings)
+        val context = DSON.compile(resource.reader().readText())
+        val obj = context.eval(mapOf("outerPayload" to outerPayload))
         println(DSON.from(obj))
     }
 }
